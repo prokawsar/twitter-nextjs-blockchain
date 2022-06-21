3bbd9c022d432b6ed459de6c170ebff00f020081
Kawsar Ahmed <prokawsar@gmail.com>
2022-06-21 09:39:49 +0600

    side bar changes

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,27 +1,26 @@
-//import Sidebar
 import Sidebar from "../components/Sidebar";
 
 const style = {
-    wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
-    content: `max-w-[1400px] w-2/3 flex justify-between`,
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`,
 };
 
 const Home = () => {
-    return (
-        <div className={style.wrapper}>
-            <div className={style.content}>
-                <div>
-                    <Sidebar />
-                </div>
-                <div>
-                    <h1>feed</h1>
-                </div>
-                <div>
-                    <h1>widgets</h1>
-                </div>
-            </div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <div>
+          <Sidebar initialSelectedIcon="Home" />
         </div>
-    );
+        <div>
+          <h1>feed</h1>
+        </div>
+        <div>
+          <h1>widgets</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
